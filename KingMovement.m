68a4fbdba85df73328c5cc1dde75b878691f8825
joelvpaul <joelvariathpaul@gmail.com>
2014-10-20 14:12:11 +0100

    Castling changes to King

--- KingMovement.m
+++ KingMovement.m
@@ -1,4 +1,4 @@
-function [possiblemoves] = KingMovement(piece_colour,chessboard,p_x,p_y)
+function [possiblemoves] = KingMovement(chessboard,piece_colour,num_moves,potential_moves,p_x,p_y)
 
 %Initialisation values --------------------------------------------------
 r_colour = piece_colour(p_x,p_y);
@@ -86,8 +86,33 @@ possiblemoves = zeros(8,8);
         end
     end
    
-   
+   %Castling 
+    
+   %For white king 
+   if (piece_colour(p_x,p_y)==119 && num_moves(p_x,p_y)==0 && num_moves(8,8)==0..... 
+           && piece_colour(8,6)==0 && piece_colour(8,7)==0 && potential_moves(8,6)==0.....  
+           && potential_moves(8,7)==0)
+       possiblemoves(8,7) = 3;  
+   end    
+   if (piece_colour(p_x,p_y)==119 && num_moves(p_x,p_y)==0 && num_moves(8,1)==0.....
+           && piece_colour(8,2)==0 && piece_colour(8,3)==0 && piece_colour(8,4)==0.... 
+           && potential_moves(8,3)==0 && potential_moves(8,4)==0)
+       possiblemoves(8,3) = 3;
+   end    
 
+   %For black king     
+   if (piece_colour(p_x,p_y)==98 && num_moves(p_x,p_y)==0 && num_moves(1,1)==0..... 
+           && piece_colour(1,2)==0 && piece_colour(1,3)==0 && piece_colour(1,4)==0..... 
+           && potential_moves(1,3)==0 && potential_moves(1,4)==0)
+       possiblemoves(1,3) = 3;
+   end    
+   if (piece_colour(p_x,p_y)==98 && num_moves(p_x,p_y)==0 && num_moves(1,8)==0..... 
+           && piece_colour(1,6)==0 && piece_colour(1,7)==0 && potential_moves(1,6)==0..... 
+           && potential_moves(1,7)==0)
+       possiblemoves(1,7) = 3;
+   end 
+   
+   
 possiblemoves(p_x,p_y)=0;
 %-------------------------------------------------------------------------
 end
