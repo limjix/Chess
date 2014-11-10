cefa9fbc000342fd954f69452361d7adc95bcec9
limjix <limjix@gmail.com>
2014-11-10 14:44:43 +0000

    Latest2

--- KingCheck.m
+++ KingCheck.m
@@ -1,17 +1,17 @@
 %KingCheck Checks if the king is in check, checkmate or stalemate
 %Colour in this case must be the current colour
-function [value]=KingCheck(chessboard,piece_colour,colour, capt_index,potentialmoves)
+function [value]=KingCheck(chessboard,piece_colour,ownkingcolour, oppcolourcapt_index,oppcolourpotentialmoves)
 
 %-------------------- King In Check --------------------------------------
-king_index = find(chessboard == 10 & piece_colour == colour);
-kingincheck = ismember(king_index,capt_index);
+king_index = find(chessboard == 10 & piece_colour == ownkingcolour);
+kingincheck = ismember(king_index,oppcolourcapt_index);
 if(kingincheck)
     value = 1;
 %Checkmate condition
-elseif kingincheck && max(max(potentialmoves))==0
+elseif kingincheck && max(max(oppcolourpotentialmoves))==0
     value = 2;
 %Stalemate Condition
-elseif kingincheck==0 && max(max(potentialmoves))==0
+elseif kingincheck==0 && max(max(oppcolourpotentialmoves))==0
     value = 3;
 %Otherwise not in check
 else
