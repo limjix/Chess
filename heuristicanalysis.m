db4eb31235524901cd9e267a3c7474552a22c297
joelvpaul <joelvariathpaul@gmail.com>
2014-11-24 14:41:35 +0000

    Changes to heuristic analysis, no checkmate

--- heuristicanalysis.m
+++ heuristicanalysis.m
@@ -13,6 +13,9 @@ if currentcolour == 119
 else
     oppcolour = 119;
 end
+
+piece_index = find(piece_colour==currentcolour);
+opp_piece_index = find(piece_colour==oppcolour);
 %ccaptureval is for the value of the piece that was captured in this game
 %state to be added to the value of the calculation
 
@@ -20,6 +23,9 @@ end
 %A move is good because it opens up capture possibilities
 num_pot_capture = length(capt_index); %Number of potential Captures
 capt_value_sum = sum(chessboard(capt_index)); %The total capture value
+
+%A move is good if it increases the number of capture 
+capt_value_diff = 51 - sum(chessboard(opp_piece_index));
 
 %------------------ Moves Analysis ---------------------------------------
 %A move is good because it opens up space for other pieces to move
@@ -35,13 +41,8 @@ opp_capt_value_sum = sum(chessboard(opp_capt_index));
 
 %------------------ Number of own pieces ---------------------------------
 %A move is good if it prevents the number of own pieces from decreasing.
-piece_index = find(piece_colour==currentcolour);
-own_piece_sum = sum(chessboard(piece_index)); 
+own_piece_sum_diff = 51 - sum(chessboard(piece_index)); 
 
-%------------------ Number of opponent pieces ----------------------------
-%A move is good if it decreases the no of opponents pieces.
-opp_piece_index = find(piece_colour==oppcolour);
-opp_piece_sum = sum(chessboard(opp_piece_index)); 
 
 %------------------- Control of centre space -----------------------------
 %A move is good if it increases control of the centre of the board
@@ -60,28 +61,25 @@ centre_space_sum =  sum(centre_piece(piece_index));
 %A move is good if it brings own pawn closer to the end of the board for promotion.
 pawn_pos = chessboard==1 & piece_colour==currentcolour;
 pawn_index = find(pawn_pos==1);
-end_dist = rem(pawn_index, 8);
-end_dist = end_dist-1;
+end_dist = 8-rem(pawn_index, 8);
+sum_pawn_pos = sum(end_dist==0);
 
 
 %------------------ Gain Factor ------------------------------------------
-gainCapture = 1;  %Encourages AI to position a piece such that it can capture more pieces in the next move
-gainMoves = 1; %Encourages AI to position such that it opens space for other pieces
-gainThreats = -1; %Discourages AI to make moves that will lead to threats
-gainOpppieces = -1; %Discourages AI from making moves that do not decrease opponents pieces
-gainOwnpieces = 1; %Discourages AI from making moves that decrease own pieces
+gainCapture = 3;  %Encourages AI to position a piece such that it can capture more pieces in the next move
+gainMoves = 10; %Encourages AI to position such that it opens space for other pieces
+gainThreats = -2.5; %Discourages AI to make moves that will lead to threats
+gainOpppieces = 30; %Discourages AI from making moves that do not decrease opponents pieces
+gainOwnpieces = -10; %Discourages AI from making moves that decrease own pieces
 gainCentre = 1; %Encourages AI to increase control of centre space
 gainOwnprom =1;
 %----------------- Final Score Calculation ------------------------------
 boardscore =  gainCapture * capt_value_sum... 
          + gainMoves * num_moves_available... 
-<<<<<<< HEAD
          + gainThreats * opp_capt_value_sum...
-         + gainCurrentCapture * ccaptureval...
-         + gainOpppieces * opp_piece_sum...
-         + gainOwnpieces * own_piece_sum...
+         + gainOpppieces * capt_value_diff...
+         + gainOwnpieces * own_piece_sum_diff...
          + gainCentre * centre_space_sum;
-=======
-         + gainThreats * opp_capt_value_sum;
->>>>>>> origin/master
+
+     boardscore = rand*boardscore;
 end
